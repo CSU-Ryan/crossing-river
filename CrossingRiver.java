85a803af17901d1e58c474011718ca2397d5319f
CSU-Ryan <enderbro@colostate.edu>
2023-08-24 12:53:45 -0600

    Begins set up of variables

---
+++ CrossingRiver.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class CrossingRiver {
+
+    String[] beforeRiver;
+    String[] afterRiver;
+    String inBoat;
+    public static void main(String[] args) {
+        init();
+    }
+
+    private void init() {
+        this.beforeRiver = new String[] {"Fox", "Chicken", "Grain"};
+        this.afterRiver = new String[] {};
+        this.inBoat = "";
+    }
+}
