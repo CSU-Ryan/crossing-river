bb5617eca9cbd60512f3993a4c85496001d5952d
CSU-Ryan <enderbro@colostate.edu>
2023-08-24 13:49:55 -0600

    Reformats variables

--- CrossingRiver.java
+++ CrossingRiver.java
@@ -2,16 +2,14 @@ import java.util.List;
 
 public class CrossingRiver {
 
-    String[] beforeRiver;
-    String[] afterRiver;
-    String inBoat;
     public static void main(String[] args) {
         init();
     }
 
-    private void init() {
-        this.beforeRiver = new String[] {"Fox", "Chicken", "Grain"};
-        this.afterRiver = new String[] {};
-        this.inBoat = "";
+    private static void init() {
+        String[] beforeRiver = new String[] {"Fox", "Chicken", "Grain"};
+        String[] afterRiver = new String[] {};
+        String inBoat = "";
     }
+    private void
 }
